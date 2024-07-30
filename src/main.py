10875cda2f207cff7de4c44177e03db843e2d650
MANIGANDANM-BITS <2022AC05144@WILP.BITS-PILANI.AC.IN>
2024-07-30 21:10:13 +0530

    Fix pylint issues: add module docstring and update constant naming

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
-# src/main.py
+"""
+This module contains a simple add function.
+"""
 
 def add(a, b):
     """Adds two numbers."""
     return a + b
 
 if __name__ == "__main__":
-    result = add(1, 2)
-    print(f"The result is {result}")
+    RESULT = add(1, 2)
+    print(f"The result is {RESULT}")
